1f9c58a6d682c7ad32a6eb030c6457799b001dbc
luan_fernandes <luanfernandes3212@gmail.com>
2026-02-10 01:32:27 -0300

    fix tierlist with contains clause

--- app/utils/supabase_utils/type_tier_list.py
+++ app/utils/supabase_utils/type_tier_list.py
@@ -4,7 +4,7 @@ def get_great_pokemon(typep: str, user_id: str):
     response = (
         SupabaseConnection.table("great_pokemon_by_type")
         .select("*")
-        .eq("tipo", typep)
+        .contains("tipo", [typep])
         .eq("user_id", user_id)
         .execute()
     )
@@ -15,7 +15,7 @@ def get_ultra_pokemon(typep: str, user_id: str):
     response = (
         SupabaseConnection.table("ultra_pokemon_by_type")
         .select("*")
-        .eq("tipo", typep)
+        .contains("tipo", [typep])
         .eq("user_id", user_id)
         .execute()
     )
@@ -26,7 +26,7 @@ def get_master_pokemon(typep: str, user_id: str):
     response = (
         SupabaseConnection.table("master_pokemon_by_type")
         .select("*")
-        .eq("tipo", typep)
+        .contains("tipo", [typep])
         .eq("user_id", user_id)
         .execute()
     )
